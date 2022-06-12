5f326d774c1dfe150b02088e5b215b1a23f88ddb
Agba Coder <yinkaabeeb@gmail.com>
2022-06-12 12:16:22 -0700

    new

--- calc.js
+++ calc.js
@@ -1,33 +1,111 @@
-//Update places count
-countAvailablePlaces(event);
+const slotId = document.querySelector("#slot-id");
+const fullname = document.querySelector("#name");
+const carModel = document.querySelector("#car-model");
+const book = document.querySelector("#book");
+const retrieveSlot = document.querySelector("#retrieveSlot");
+const slotSelected = document.querySelector("#slots-selected");
+const retrieveModel = document.querySelector("#retrieveModel");
+const retrieveName = document.querySelector("#retrieveName");
+const price = document.querySelector("#price");
+const time = document.querySelector("#time");
+const total = document.querySelector("#total");
+const tabulate = document.querySelector("#tabulate");
+const bookingsFromLocalStorage = JSON.parse(localStorage.getItem("booking"));
+const slotsFromLocalStorage = JSON.parse(localStorage.getItem("filledSlots"));
+let booking;
+let filledSlots;
 
-// Empty text box
-document.querySelector("#carValue").value = '';
-
-// Render the table
-renterTable();
-
-const freeSpot = (event) => {
-cars.splice(event.target.dataset.row, 1);
-setClassForBadge();
-
-if (cars.length == totalPlaces) {
-  document.querySelector('#carButton').setAttribute('disabled');
-} else {
-  document.querySelector('#carButton').removeAttribute('disabled');
+// If local storage is empty, initialize an empty booking and filledSlots array
+if(bookingsFromLocalStorage){
+    booking = [...bookingsFromLocalStorage];
+    filledSlots = [...slotsFromLocalStorage];
+}
+else{
+    booking = [];
+    filledSlots = [];
 }
 
-// Render Table again after delete 
-renterTable();
-//Update places count
-countAvailablePlaces(event);
-}
+document.querySelector("#filled-slots").innerHTML = "FILLED SLOTS: "+JSON.stringify(filledSlots);
 
-// Add new car to the array
-addCarButton.addEventListener('click', addCar);
+// event listener to add booking information to local storage
+book.addEventListener("click", () => {
 
-// Render Table
-renterTable();
+    // get the current time which would be the entry time
+    const currentDate = new Date();
+    const currentTime = currentDate.getTime() / 1000;
 
-//Show places count at page load
-countAvailablePlaces(event);
+    // calculating the price dependent on slot
+    let slotPrice;
+    if(slotId.value > 10){
+        slotPrice = 100;
+    }
+    else{
+        slotPrice = 60;
+    }
+
+    // push the booking information to the array of booked slots
+    booking.push({
+        "slot": slotId.value,
+        "name": fullname.value,
+        "carModel": carModel.value,
+        "price": slotPrice,
+        "entryTime": currentTime
+    });
+    // set to local storage
+    localStorage.setItem("booking", JSON.stringify(booking));
+
+    // add slot to filledSlot array
+    filledSlots.push(slotId.value);
+    localStorage.setItem("filledSlots", JSON.stringify(filledSlots));
+
+    // alert user and reload window
+    alert("booking was successful!");
+    window.location.reload();
+});
+
+
+// add eventListener to calculate and fill table values
+tabulate.addEventListener("click", () => {
+
+    let userBooking;
+
+    // to retrieve bookings
+    for(let i = 0; i < bookingsFromLocalStorage.length; i++){
+        if(bookingsFromLocalStorage[i].slot === retrieveSlot.value){
+            userBooking = bookingsFromLocalStorage[i];
+        }
+    }
+
+    // calculate the time spent using the difference between entry time and now
+    const entryTime = userBooking.entryTime;
+    const exitDate = new Date();
+    const exitTime = exitDate.getTime() / 1000;
+    const timeSpent = exitTime - entryTime;
+
+    // convert time into hour, minutes
+    const sec = parseInt(timeSpent, 10); // convert value to number if it's string
+    let hours   = Math.floor(sec / 3600); // get hours
+    let minutes = Math.floor((sec - (hours * 3600)) / 60); // get minutes
+    let seconds = sec - (hours * 3600) - (minutes * 60); //  get seconds
+    // add 0 if value < 10; Example: 2 => 02
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    const formattedTime = hours+':'+minutes+':'+seconds; 
+
+    // calculate the total bills in case user spends above 30mins
+    let totalBills;
+    if(timeSpent <= 1800) {
+        totalBills = userBooking.price;
+    }
+    else{
+        totalBills = userBooking.price + (hours * 15);
+    }
+
+    // update information in the table
+    slotSelected.innerHTML = userBooking.slot;
+    retrieveName.innerHTML = userBooking.name;
+    retrieveModel.innerHTML = userBooking.carModel;
+    price.innerHTML = userBooking.price+'$';
+    time.innerHTML = formattedTime;
+    total.innerHTML = totalBills+'$';)
